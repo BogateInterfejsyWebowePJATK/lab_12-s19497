ca7f03575c4d6bb4b4e318ac68d126c7477422f5
jan <jannos1999@gmail.com>
2021-01-18 18:18:52 +0100

    zad_2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import MyForm from "./components/MyForm";
+import Table from "./components/Table";
+import {useState} from "react";
 
 function App() {
+    const [tableProps, setTableProps] = useState({});
     return (
         <div className="App">
-            <MyForm/>
+            <MyForm setTableProps={setTableProps}/>
+            <Table {...tableProps}/>
         </div>
     );
 }
--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,17 +1,59 @@
 import {useState} from 'react';
 
-function avg(...args) {
-    return args.map(i => Number(i)).reduce((i, j) => i + j) / args.length;
-}
-
-function MyForm() {
+function MyForm({setTableProps}) {
     const [inputText, setInputText] = useState('');
-    const [outputText, setOutputText] = useState('0');
+    const [inputText2, setInputText2] = useState('');
+    const [select, setSelect] = useState('Red');
+    const [radio, setRadio] = useState('Yes');
+    const [checked, setChecked] = useState(false);
+
+    function _onChange(setter) {
+        return event => {
+            let target = event.target;
+            console.log(target, target.value);
+            setter(target.type === 'checkbox' ? target.checked : target.value);
+        }
+    }
+
     return (
         <div className="MyForm">
-            <h1>{outputText}</h1>
-            <input type="text" value={inputText} onChange={(event => setInputText(event.target.value))}/>
-            <button onClick={() => setOutputText(avg(...inputText.split(' ')))}>Average</button>
+            <h1>Yo!</h1>
+            <label>
+                Input 1
+                <input type="text" value={inputText} onChange={_onChange(setInputText)}/>
+            </label>
+            <label>
+                Input 2
+                <input type="text" value={inputText2} onChange={_onChange(setInputText2)}/>
+            </label>
+            <div onChange={_onChange(setRadio)}>
+                <label>
+                    Yes
+                    <input type="radio" name="answer" value="Yes" checked={radio === "Yes"}/>
+                </label>
+                <label>
+                    No
+                    <input type="radio" name="answer" value="No" checked={radio === "No"}/>
+                </label>
+            </div>
+            <label>
+                Select
+                <select onChange={_onChange(setSelect)}>
+                    <option>Red</option>
+                    <option>Blue</option>
+                    <option>Green</option>
+                </select>
+            </label>
+            <label>
+                Check me
+                <input type="checkbox" onChange={_onChange(setChecked)}/>
+            </label>
+            <button onClick={() => setTableProps({
+                radio, select, checked, inputText, inputText2
+            })
+            }>Submit
+            </button>
+            <button onClick={() => setTableProps({})}>Reset</button>
         </div>
     )
 }
